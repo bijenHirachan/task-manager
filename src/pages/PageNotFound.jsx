7faf938ff14ff82f9b0e73b70fadffcd2366fc3a
Bijen Hirachan <bijen.hirachan@gmail.com>
2024-02-06 12:15:20 +0100

    404 page updated

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const PageNotFound = () => {
-  return <div>PageNotFound</div>;
+  return (
+    <div className="min-h-[100svh] flex flex-col gap-8 justify-center items-center">
+      <h3 className="text-four text-5xl tracking-widest leading-7">404</h3>
+
+      <p className="text-2xl tracking-wider text-four leading-7">
+        Page Not Found
+      </p>
+
+      <Link to={"/"} className="text-blue-400  tracking-wide hover:underline">
+        Return to home
+      </Link>
+    </div>
+  );
 };
 
 export default PageNotFound;
